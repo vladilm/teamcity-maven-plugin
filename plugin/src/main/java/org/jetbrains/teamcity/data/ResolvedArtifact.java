ce26029d623e09451ef4a29bc4907d76e3712f74
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-01-25 19:14:40 +0100

    Fix inspections in `ResolvedArtifact`

--- plugin/src/main/java/org/jetbrains/teamcity/data/ResolvedArtifact.java
+++ plugin/src/main/java/org/jetbrains/teamcity/data/ResolvedArtifact.java
@@ -1,14 +1,14 @@
 package org.jetbrains.teamcity.data;
 
+import lombok.Getter;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.eclipse.aether.artifact.Artifact;
+import org.jetbrains.teamcity.Jdk8Compat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
@@ -22,6 +22,7 @@ public class ResolvedArtifact {
     private  static final Logger LOG = LoggerFactory.getLogger(ResolvedArtifact.class);
 
     private final org.eclipse.aether.artifact.Artifact source;
+    @Getter
     private final boolean reactorProject;
 
     public ResolvedArtifact(Artifact source, boolean isReactorProject) {
@@ -29,37 +30,49 @@ public class ResolvedArtifact {
         this.reactorProject = isReactorProject;
     }
 
-    public boolean isReactorProject() {
-        return reactorProject;
-    }
+    private static final String AgentPluginNameKey = "AGENT_PLUGIN_NAME";
 
     public String getFileName() {
-        String name = source.getFile().getName();
-        if (source.getClassifier() != null && Objects.equals("teamcity-agent-plugin", source.getClassifier())) {
+        File sourceFile = source.getFile();
+        String name = sourceFile.getName();
+        if (Objects.equals("teamcity-agent-plugin", source.getClassifier())) {
             name = source.getArtifactId() + "." + source.getExtension();
             try {
-                if (source.getFile().exists()) {
-                    ZipFile file = new ZipFile(source.getFile());
-                    ZipEntry pluginDescriptorEntry = file.getEntry("teamcity-plugin.xml");
-                    if (pluginDescriptorEntry != null) {
+                if (sourceFile.exists()) {
+                    try (ZipFile file = new ZipFile(sourceFile)) {
+                        ZipEntry pluginDescriptorEntry = file.getEntry("teamcity-plugin.xml");
+                        if (pluginDescriptorEntry == null) {
+                            return name;
+                        }
                         InputStream inputStream = file.getInputStream(pluginDescriptorEntry);
                         BufferedReader isr = new BufferedReader(new InputStreamReader(inputStream));
-                        Map<String, String> map = isr.lines().flatMap(ResolvedArtifact::lookupPluginName).collect(Collectors.toMap(it -> it.getKey(), it -> it.getValue()));
-                        if (map.get("AGENT_PLUGIN_NAME") != null) {
-                            name = map.get("AGENT_PLUGIN_NAME") + "." + source.getExtension();
+                        List<Map.Entry<String, String>> entries = isr.lines()
+                                .flatMap(ResolvedArtifact::lookupPluginName)
+                                .filter(e -> AgentPluginNameKey.equals(e.getKey()))
+                                .collect(Collectors.toList());
+                        if (entries.size() > 1) {
+                            String values = entries.stream().map(Map.Entry::getValue)
+                                    .collect(Collectors.joining(","));
+                            throw new IllegalStateException(String.format(
+                                    "Duplicate key %s, values are: %s", AgentPluginNameKey, values));
+                        }
+                        String agentPluginName = entries.size() == 1 ? entries.get(0).getValue() : null;
+                        if (Jdk8Compat.isNotEmpty(agentPluginName)) {
+                            name = agentPluginName + "." + source.getExtension();
                         }
                     }
                 }
             } catch (IOException e) {
-                LOG.warn("Error while fetching agent plugin name of " +  source.getFile(), e);
+                LOG.warn("Error while fetching agent plugin name of {}", sourceFile, e);
             }
         }
         return name;
     }
 
+    private static final Pattern AssignPattern = Pattern.compile("@@([\\s\\-\\w]+)=([\\s\\w\\-]+)@@");
+
     public static Stream<Map.Entry<String, String>> lookupPluginName(String line) {
-        Pattern pat = Pattern.compile("@@([\\s\\-\\w]+)=([\\s\\w\\-]+)@@");
-        Matcher matcher = pat.matcher(line);
+        Matcher matcher = AssignPattern.matcher(line);
         if (matcher.find()) {
             return Stream.of(new ImmutablePair<>(matcher.group(1), matcher.group(2)));
         }
