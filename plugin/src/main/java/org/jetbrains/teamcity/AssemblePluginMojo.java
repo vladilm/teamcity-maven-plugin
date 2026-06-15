848e55370cb529c80c984756d53eabad3fa5013a
Vladimir I <vladimir.ilmov@jetbrains.com>
2026-06-15 13:53:02 +0200

    Skip-fast if no agent/server configurations are found in module

--- plugin/src/main/java/org/jetbrains/teamcity/AssemblePluginMojo.java
+++ plugin/src/main/java/org/jetbrains/teamcity/AssemblePluginMojo.java
@@ -110,6 +110,8 @@ public class AssemblePluginMojo extends BaseTeamCityMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("TeamCity Assemble start");
+        if (!agent.isNeedToBuild() && !server.isNeedToBuild())
+            return;
         setDefaultconfigurationValues(pluginVersion);
         try {
             IncrementalAssembleSupport incrementalSupport = new IncrementalAssembleSupport(
